3df7f7e5d940ca291fa6e9694e66628614a3fa1a
pdm2017 <pdm96@hanmail.net>
2019-09-26 14:27:36 +0900

    Update DataHandling.py

--- DataHandling.py
+++ DataHandling.py
@@ -15,3 +15,23 @@ def replaceOutlierToLimit(s):
     tmpS = np.where(s < low_fense, low_fense, s)
     tmpS = np.where(tmpS > hig_fense, hig_fense, tmpS)
     return tmpS
+
+#------------------------------------------------------------
+# 문자열로 되어 있는 컬럼을 Pandas의 DateTime 형으로 변환한다
+#------------------------------------------------------------
+data['DATETIME'] = pd.to_datetime(data['DATA2'], format = '%y-%m-%d %H:%M')
+data['YEAR'] = pd.DatetimeIndex(data['DATETIME']).year
+data['MONTH'] = pd.DatetimeIndex(data['DATETIME']).month
+data['DATE'] = pd.DatetimeIndex(data['DATETIME']).day
+data['DAY'] = pd.DatetimeIndex(data['DATETIME']).dayofweek
+data['HOUR'] = pd.DatetimeIndex(data['DATETIME']).hour
+data['MINUTE'] = pd.DatetimeIndex(data['DATETIME']).minute
+
+# Pandas의 DataTime 형을 숫자로 변환한다
+data['DATE_NUM'] = pd.to_numeric(data['DATETIME'], downcast='integer')
+
+# 해당 컬럼에서 최소한의 값을 찾는다
+_min_ = data['DATE_NUM'].min()
+
+# 가장 최소값으로 빼주고 분단위 이므로 60 초를 나눠서 1분단위로 증가 시켜준다
+data['DATE_SORT'] = (data['DATE_NUM'] - _min_) / (1000000000 * 60)
